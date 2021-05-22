aaa0dba8d599f1075634ce843e4f158ad97011f9
njmbb8 <nmcgr001@gmail.com>
2021-05-22 16:42:56 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const tagToRemove = document.getElementsByTagName('main')[0];
+tagToRemove.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'is the champion';
+document.body.appendChild(newHeader);
